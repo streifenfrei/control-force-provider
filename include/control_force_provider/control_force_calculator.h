4bdd7bf6e6b264f6de174fa5e0e721743e86808b
Martin Lelis <martin.lelis@nct-dresden.de>
2022-06-28 16:57:03 +0200

    undo setting RCM as goal

--- include/control_force_provider/control_force_calculator.h
+++ include/control_force_provider/control_force_calculator.h
@@ -21,21 +21,14 @@ class ControlForceCalculator {
  public:
   explicit ControlForceCalculator(boost::shared_ptr<Obstacle>& obstacle) : obstacle_(obstacle) {}
   void getForce(Eigen::Vector4d& force, const Eigen::Vector3d& ee_position) {
-    if (goal_available_)
-      getForceImpl(force, ee_position);
-    else
-      force = {};
+    if (!goal_available_) setGoal(ee_position);
+    getForceImpl(force, ee_position);
   }
   virtual ~ControlForceCalculator() = default;
   [[nodiscard]] const Eigen::Vector3d& getRCM() const { return rcm_; }
-  void setRCM(const Eigen::Vector3d& rcm) {
-    rcm_ = rcm;
-    goal_available_ = true;
-    setGoal(rcm_);
-  };
+  void setRCM(const Eigen::Vector3d& rcm) { rcm_ = rcm; };
   [[nodiscard]] const Eigen::Vector3d& getGoal() const { return goal_; }
   void setGoal(const Eigen::Vector3d& goal) {
-    ROS_WARN_STREAM("Set goal");
     goal_available_ = true;
     goal_ = goal;
   };
