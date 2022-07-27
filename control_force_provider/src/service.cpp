8bbb798633afd8c56d16a37a8149006117a5ddb1
Martin Lelis <martin.lelis@nct-dresden.de>
2022-07-27 12:35:43 +0200

    make service interruptable

--- control_force_provider/src/service.cpp
+++ control_force_provider/src/service.cpp
@@ -12,12 +12,9 @@ using namespace boost;
 
 const char* socket_file = "/tmp/cfp_uds";
 const int vector_size = 4 * sizeof(double);
-void* data;
+bool stop = false;
 
-void cleanup(int signum) {
-  free(data);
-  remove(socket_file);
-}
+void sigint_handler(int signum) { stop = true; }
 
 bool setRealtimePriority() {
   const int thread_priority = sched_get_priority_max(SCHED_FIFO);
@@ -37,20 +34,23 @@ bool setRealtimePriority() {
 int main(int argc, char* argv[]) {
   control_force_provider::ControlForceProvider cfp;
   asio::io_context io_context_{};
-  remove(socket_file);
   asio::local::stream_protocol::endpoint uds_ep_{socket_file};
   asio::local::stream_protocol::acceptor acceptor(io_context_, uds_ep_);
   asio::local::stream_protocol::socket uds_socket_{io_context_};
   acceptor.accept(uds_socket_);
-  data = malloc(vector_size);
-  signal(SIGTERM, cleanup);
+  void* data = malloc(vector_size);
   asio::mutable_buffer input_buffer(data, vector_size);
   if (setRealtimePriority()) ROS_INFO_STREAM_NAMED("control_force_provider/service", "Successfully set realtime priority for current thread.");
-  while (true) {  // TODO make loop interruptable. Does not work currently as read() blocks and async_read() would spawn a non RT thread;
-    read(uds_socket_, input_buffer);
-    Eigen::Vector4d ee_position((double*)input_buffer.data());
-    Eigen::Vector4d force;
-    cfp.getForce(force, ee_position);
-    write(uds_socket_, asio::const_buffer((const void*)force.data(), vector_size));
+  signal(SIGINT, sigint_handler);
+  while (!stop) {
+    if (uds_socket_.available() >= vector_size) {
+      read(uds_socket_, input_buffer);
+      Eigen::Vector4d ee_position((double*)input_buffer.data());
+      Eigen::Vector4d force;
+      cfp.getForce(force, ee_position);
+      write(uds_socket_, asio::const_buffer((const void*)force.data(), vector_size));
+    }
   }
+  remove(socket_file);
+  free(data);
 }
