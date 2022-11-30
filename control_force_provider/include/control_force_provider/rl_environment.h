975023f46d7b68bba6db8c2b72d7de8cb50b1dc2
Martin Lelis <martin.lelis@nct-dresden.de>
2022-11-30 18:31:40 +0100

    implement basic rl environment

--- control_force_provider/include/control_force_provider/rl_environment.h
+++ control_force_provider/include/control_force_provider/rl_environment.h
@@ -2,14 +2,24 @@
 
 #include <torch/torch.h>
 
+#include "control_force_calculator.h"
+#include "control_force_provider.h"
+
 namespace control_force_provider::backend {
 class TorchRLEnvironment {
  private:
+  torch::Tensor ee_positions_;
+  double interval_duration_;
+  unsigned int batch_size_;
+  boost::shared_ptr<ManualTime> time_;
+  boost::shared_ptr<Environment> env_;
+  boost::shared_ptr<EpisodeContext> episode_context_;
+  boost::shared_ptr<StateProvider> state_provider_;
   torch::Tensor state_;
 
  public:
-  TorchRLEnvironment();
+  TorchRLEnvironment(const std::string& config_file);
   void update(const torch::Tensor& actions);
-  const torch::Tensor& getState();
+  torch::Tensor getState();
 };
 }  // namespace control_force_provider::backend
--- control_force_provider/src/rl_environment.cpp
+++ control_force_provider/src/rl_environment.cpp
@@ -2,21 +2,35 @@
 
 #include <torch/extension.h>
 
+using namespace control_force_provider;
 using namespace torch;
 
 namespace control_force_provider::backend {
-TorchRLEnvironment::TorchRLEnvironment() {
-  auto options = TensorOptions().dtype(kFloat64);
-  state_ = torch::ones(90, options);
+TorchRLEnvironment::TorchRLEnvironment(const std::string& config_file) {
+  YAML::Node config = YAML::LoadFile(config_file);
+  Time::setType<ManualTime>();
+  time_ = boost::static_pointer_cast<ManualTime>(Time::getInstance());
+  env_ = boost::make_shared<Environment>(config);
+  episode_context_ = boost::make_shared<EpisodeContext>(env_->obstacles, env_->obstacle_loader, config);
+  state_provider_ = boost::make_shared<StateProvider>(*env_, utils::getConfigValue<std::string>(config["rl"], "state_pattern")[0]);
+  interval_duration_ = utils::getConfigValue<double>(config["rl"], "interval_duration")[0];
+  ee_positions_ = torch::zeros({batch_size_, 3}, utils::getTensorOptions());
 }
 
-void TorchRLEnvironment::update(const Tensor& actions) {}
+void TorchRLEnvironment::update(const Tensor& actions) {
+  torch::Tensor actions_copy = actions;
+  env_->clipForce(actions_copy);
+  ee_positions_ += actions_copy * interval_duration_;
+  // TODO: get new episodes
+  env_->update(ee_positions_);
+  *time_ += interval_duration_ * 1e-3;
+}
 
-const Tensor& TorchRLEnvironment::getState() { return state_; }
+Tensor TorchRLEnvironment::getState() { return state_provider_->createState(); }
 
 PYBIND11_MODULE(native, module) {
   py::class_<TorchRLEnvironment>(module, "RLEnvironment")
-      .def(py::init<>())
+      .def(py::init<std::string>())
       .def("update", &TorchRLEnvironment::update)
       .def("getState", &TorchRLEnvironment::getState);
 }
